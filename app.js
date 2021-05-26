1f17735a2a4161e3306c775a006924cf68c005a6
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-05-26 16:25:34 +0530

    update app.js

--- app.js
+++ app.js
@@ -5,18 +5,31 @@ const startBtn = document.querySelector(".startBtn");
 const stopBtn = document.querySelector(".stopBtn");
 const resetBtn = document.querySelector(".resetBtn");
 let timerTime = 00;
+let interval;
+let isrunning = false;
 
 //functions
 
 setInterval(function () {}, 1000);
 
 function startTimer() {
+  if (isrunning) return;
+
+  isrunning = true;
   interval = setInterval(incrementTimer, 1000);
 }
 function stopTimer() {
+  if (!isrunning) return;
+
+  isrunning = false;
   clearInterval(interval);
 }
-function resetTimer() {}
+function resetTimer() {
+  stopTimer();
+  timerTime = 0;
+  minutesTimer.textContent = "00";
+  secondsTimer.textContent = "00";
+}
 
 function pad(number) {
   return number < 10 ? `0` + number : number;
